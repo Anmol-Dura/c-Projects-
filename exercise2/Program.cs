664e69a5282962c53f60406f957af3a89bc104c3
Anmol Dura <anmoldura8@gmail.com>
2024-02-11 16:59:08 -0500

    finised the program

--- exercise2/Program.cs
+++ exercise2/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Runtime.Serialization;
+
+Console.Write("Enter a number: ");
+int userNumber = int.Parse(Console.ReadLine());
+string evenOdd = " ";
+if (userNumber % 2 == 0)
+{
+    evenOdd = "even";
+}
+else
+{
+    evenOdd = "Odd";
+}
+
+if (userNumber > 0)
+{
+    Console.WriteLine("The number is {0} and positive",evenOdd );
+}
+else if (userNumber < 0)
+{
+    Console.WriteLine("The number is {0} and negetive",evenOdd);
+}
+else
+{
+    Console.WriteLine("The number is {0} and nutral",evenOdd);
+}
